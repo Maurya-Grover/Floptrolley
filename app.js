91ffa6f68113bd8f7c1ec5e6c0a7ef16e6953f66
Maurya-Grover <maurya.grover@gmail.com>
2022-02-04 06:38:27 +0530

    server start

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
-const express = require("express");
+const express = require('express');
 const app = express();
+
+app.use((req, res, next) => {
+	res.status(201).json({
+		message: 'Connection Successful',
+	});
+});
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
-const http = require("http");
+const http = require('http');
+const app = require('./app');
 const port = process.env.PORT || 3000;
-const server = http.createServer();
+const server = http.createServer(app);
 
+console.log('Server Started');
 server.listen(port);
