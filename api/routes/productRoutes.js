da71a0d5fe9952b532830893ed61ac486d7abded
Maurya-Grover <maurya.grover@gmail.com>
2022-02-05 10:33:45 +0530

    Add More Response Data

--- api/routes/productRoutes.js
+++ api/routes/productRoutes.js
@@ -8,8 +8,13 @@ router.get('/', (req, res, next) => {
 	});
 });
 router.post('/', (req, res, next) => {
+	const product = {
+		name: req.body.productName,
+		price: req.body.price,
+	};
 	res.status(201).json({
 		message: 'Handled POST request to /products route',
+		createdProduct: product,
 	});
 });
 
